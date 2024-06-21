181844a68bea8a2785dfabb4991212a1c7f6dc2b
Dhritiman Sagar <dhrits100@gmail.com>
2024-06-20 21:29:02 -0400

    Clean up client code

--- dog_breed_id/gradio_client.py
+++ dog_breed_id/gradio_client.py
@@ -6,7 +6,7 @@ __all__ = ['predict_image']
 # %% ../nbs/08_client.ipynb 2
 from gradio_client import Client, handle_file
 def predict_image(client, impath):
-    """Given impath, returns a PIL image with the annotated bounding box and probabilities"""
+    """Given impath, returns a PIL annotated image, breed and confidence"""
     result = client.predict(
 		img=handle_file(impath),
 		api_name="/predict"
--- hf_cient.py
+++ hf_cient.py
@@ -1,8 +1,18 @@
 from gradio_client import Client, handle_file
+import argparse
 
-client = Client("deman539/dog_breed_id")
-result = client.predict(
-		img=handle_file('https://raw.githubusercontent.com/gradio-app/gradio/main/test/test_files/bus.png'),
-		api_name="/predict"
-)
-print(result)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        prog='Simple client for dog breed identification API',
+        description='Given an image path, identifies the dog breed',
+    )
+    parser.add_argument('-i', '--impath', help='path or url to input image with dog breed to identify', required=True)
+    args = parser.parse_args()
+
+    client = Client("deman539/dog_breed_id")
+    annotated_impath, breed, confidence = client.predict(
+        img=handle_file(args.impath),
+        api_name="/predict"
+    )
+    print(f'Annotated image: {annotated_impath}')
+    print(f'Breed: {breed["label"]}, confidence: {confidence*100:.2f}%')
